9a4ba092e203dac6ca49da962fd383a91fd3c379
Brisset Joachim <joachim.brisset@eisti.fr>
2021-06-05 20:32:58 +0200

    small Fix 404 page

--- controllers/404.php
+++ controllers/404.php
@@ -5,6 +5,5 @@ require_once "../models/Session.php";
 require_once "../models/Authentication.php";
 
 if (Authentication::isAuth()['auth']) Session::extendValidity();
-$lastUrl = Session::popHistory();
 
 include "../views/404.php";
