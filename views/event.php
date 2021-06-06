e96a2ce25777458248020c53f982b914914d3b55
Brisset Joachim <joachim.brisset@eisti.fr>
2021-06-07 01:42:56 +0200

    Fix when end_at equal 0000-00-00

--- views/event.php
+++ views/event.php
@@ -48,7 +48,11 @@ $reponse = $bdd->query('SELECT * FROM events');
 { ?>
         <div class="scrollable annonce">
             <h2><?php echo $donnees["name"] ?></h2>
-            <p><?php echo $donnees["description"]?> <br> date :<?php echo $donnees["start_at"] ?><?php if (!empty($donnees["end_at"])){echo " au " ,$donnees["end_at"];} ?> <br> lieu : <?php echo $donnees["place"] ?> <br> places : <?php if (!empty($donnees["place_number"])){echo $donnees["place_number"] ;}else {echo "illimité";}  ?></p>
+            <p><?php echo $donnees["description"]?> <br>
+                date : <?php echo $donnees["start_at"] ?><?php if (!empty($donnees["end_at"]) && $donnees['end_at'] != "0000-00-00"){echo " au " ,$donnees["end_at"];} ?> <br>
+                lieu : <?php echo $donnees["place"] ?> <br>
+                places : <?php if (!empty($donnees["place_number"])){echo $donnees["place_number"] ;}else {echo "illimité";}  ?>
+            </p>
 
         </div>
 
