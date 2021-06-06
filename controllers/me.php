779a32c3c71b2d02fcb98a720c7504df89c1770d
Brisset Joachim <joachim.brisset@eisti.fr>
2021-06-06 16:46:35 +0200

    Added Logout

--- controllers/me.php
+++ controllers/me.php
@@ -3,6 +3,15 @@
 require_once "../models/Session.php";
 require_once "../models/Authentication.php";
 
+session_start();
+
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+    $_SESSION = [];
+    session_regenerate_id(true);
+    header('Location: /');
+    die;
+}
+
 Session::appendToHistory();
 if (Authentication::isAuth()['auth']) {
     Session::extendValidity();
