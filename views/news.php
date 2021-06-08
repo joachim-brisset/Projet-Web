1f5252a8b5daf112536c4af545a55b7698db02ce
robert <thelava12@gmail.com>
2021-06-08 11:19:44 +0200

    link news to bdb

--- views/news.php
+++ views/news.php
@@ -2,22 +2,20 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet">
-
     <link rel="stylesheet" href="/css/reset.css">
     <link rel="stylesheet" href="/css/global.css">
     <link rel="stylesheet" href="/css/header.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/Scrollbar.css">
 
-    <link rel="stylesheet" href="/css/home.css">
-
+    <link rel="stylesheet" href="/css/news.css">
 
     <script src="/js/header-app.js"></script>
-    <title> Basket Passion | Home </title>
+    <title>Basket Passion | Actualités</title>
 </head>
-
 
 <?php
 try
@@ -29,43 +27,18 @@ catch (Exception $e)
         die('Erreur : ' . $e->getMessage());
 }
 
-$reponse = $bdd->query('SELECT * FROM articles ORDER BY created_at LIMIT 4 ');
+$reponse = $bdd->query('SELECT * FROM articles ORDER BY created_at  ');
 
 
 ?>
-
-
 
 <body class="scrollable">
 <?php include '../controllers/header.php' ?>
 
 <main>
-    <section id="cover">
-        <img src="/img/background.jpeg" alt="basketball cover photo">
-
-        <div id="cover-bottom">
-            <div class="advertisement-overlay">
-                <div class="circle n1"></div>
-                <div class="circle n2"></div>
-
-                <div class="content">
-                    <img id="ball" src="/img/qdqzdqdqzdqz.png" />
-                    <div>
-                        <p>Nouvelle balle en collaboration avec <span class="uppercase">supraw</span>®</p>
-                        <p class="uppercase">dispo dans la boutique</p>
-                    </div>
-                </div>
-            </div>
-
-            <a class="scroll-down" href="#news">
-                <ion-icon name="arrow-down-circle-outline"></ion-icon>
-            </a>
-        </div>
-    </section>
-
-    <section id="news">
+<section id="news">
         <h1> News </h1>
-        <div class="scrollable">
+        <div >
 
             <?php while ($item = $reponse->fetch()) :?>
                 <div class="news">
@@ -78,13 +51,13 @@ $reponse = $bdd->query('SELECT * FROM articles ORDER BY created_at LIMIT 4 ');
                     </div>
                 </div>
             <?php endwhile; ?>
-            <button id="loadMoreButton"> charger plus ! </button>
+            
         </div>
     </section>
 </main>
 
-<?php include '../views/components/footer.html' ?>
 </body>
-<script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+
 
 </html>
